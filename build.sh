7c6f8104c059c1bb2264d2797d8c7e2f327dd91f
Tony <yong1236@foxmail.com>
2015-11-29 22:08:23 +0800

    -

---
+++ build.sh
@@ -0,0 +1,4 @@
+grunt build
+cp dist/styles/ ../../phpstorm/tonysite-theme-one/ -rf
+cp dist/scripts/ ../../phpstorm/tonysite-theme-one/ -rf
+cp dist/bower_components/ ../../phpstorm/tonysite-theme-one/ -rf
